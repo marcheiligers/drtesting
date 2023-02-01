0fad62b0ccb43185fcfcd504c232d7e7a3420979
Marc Heiligers <marc@eternal.co.za>
2023-02-01 07:44:30 -0700

    Add multiple possibe correct solutions

--- app/tests.rb
+++ app/tests.rb
@@ -10,7 +10,7 @@ TEST_FRUIT_DATAS = [
   {
     fruits: {
     },
-    expected_sort_fruits: [:none]
+    possible_sort_fruits: [[:none]]
   }, {
     fruits: {
       apples: {parent: :oranges, v1: 1, v2: 2},
@@ -18,7 +18,10 @@ TEST_FRUIT_DATAS = [
       bananas: {parent: :none, v1: 1, v2: 2},
       lemons: {parent: :bananas, v1: 1, v2: 2},
     },
-    expected_sort_fruits: [:none, :bananas, :oranges, :lemons, :apples]
+    possible_sort_fruits: [
+      [:none, :bananas, :oranges, :lemons, :apples],
+      [:none, :bananas, :lemons, :oranges, :apples]
+    ]
   }, {
     # This test case fails, because the sorting depends on order and :bananas will come before :pears
     fruits: {
@@ -28,13 +31,33 @@ TEST_FRUIT_DATAS = [
       lemons: {parent: :bananas, v1: 1, v2: 2},
       pears: {parent: :none, v1: 1, v2: 2},
     },
-    expected_sort_fruits: [:none, :pears, :bananas, :oranges, :lemons, :apples]
+    possible_sort_fruits: [
+      [:none, :pears, :bananas, :oranges, :lemons, :apples],
+      [:none, :bananas, :pears, :oranges, :lemons, :apples]
+    ]
+  }, {
+    # This test case fails on purpose to show the failure message example (there is no :blah)
+    fruits: {
+      apples: {parent: :oranges, v1: 1, v2: 2},
+      oranges: {parent: :bananas, v1: 1, v2: 2},
+      bananas: {parent: :none, v1: 1, v2: 2},
+      lemons: {parent: :bananas, v1: 1, v2: 2},
+      pears: {parent: :none, v1: 1, v2: 2},
+    },
+    possible_sort_fruits: [
+      [:blah, :pears, :bananas, :oranges, :lemons, :apples],
+      [:blah, :bananas, :pears, :oranges, :lemons, :apples]
+    ]
   }
 ]
 
 def test_sort_fruits(_args, assert)
   TEST_FRUIT_DATAS.each.with_index do |data, i|
-    assert.equal!(sort_fruits(data[:fruits]), data[:expected_sort_fruits], "sort_fruits test case #{i} failed")
+    result = sort_fruits(data[:fruits])
+    assert.true!(
+      data[:possible_sort_fruits].any? { |pos| pos == result },
+      "sort_fruits test case #{i} failed:\n#{result}\nnot found in\n#{data[:possible_sort_fruits]}"
+    )
   end
 end
 
